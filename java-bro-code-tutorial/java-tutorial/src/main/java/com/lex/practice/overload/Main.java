a095b7c4bf836dab6f5d6c048958300e6f7c1648
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-03-02 23:48:58 +0800

    pre toString method

--- java-bro-code-tutorial/java-tutorial/src/main/java/com/lex/practice/overload/Main.java
+++ java-bro-code-tutorial/java-tutorial/src/main/java/com/lex/practice/overload/Main.java
@@ -11,5 +11,21 @@ public class Main {
         // but have different parameters
         // name + parameters = signature
 
+        Pizza pizza = new Pizza("thicc crust","tomato","mozzarella","pepperoni");
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+
+        Pizza pizza2 = new Pizza("thicc crust","tomato","mozzarella");
+
+        Pizza pizza3 = new Pizza("thicc crust","tomato");
+
+        Pizza pizza4 = new Pizza("thicc crust");
+
+        Pizza pizza5 = new Pizza();
+
+
     }
 }
---
+++ java-bro-code-tutorial/java-tutorial/src/main/java/com/lex/practice/overload/Pizza.java
@@ -0,0 +1,37 @@
+package com.lex.practice.overload;
+
+/**
+ * @author : LEX_YU
+ * @date : 02/03/2023
+ */
+public class Pizza {
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+
+    public Pizza() {
+    }
+
+    public Pizza(String bread) {
+        this.bread = bread;
+    }
+
+    public Pizza(String bread, String sauce) {
+        this.bread = bread;
+        this.sauce = sauce;
+    }
+
+    public Pizza(String bread, String sauce, String cheese) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
+
+    public Pizza(String bread, String sauce, String cheese, String topping) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+}
