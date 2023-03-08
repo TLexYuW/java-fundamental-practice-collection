8584fe6f7f3e6bc59be3291176f3ec69d1650951
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-03-08 22:04:43 +0800

    in baseUri...

--- Java Programming Masterclass updated to Java 17 (Udemy)/java-networking/src/main/java/com/lex/practice/high_level_api/Main.java
+++ Java Programming Masterclass updated to Java 17 (Udemy)/java-networking/src/main/java/com/lex/practice/high_level_api/Main.java
@@ -16,14 +16,22 @@ public class Main {
 //            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
 //            URI uri = new URI("https://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
             URI baseUri = new URI("https://username:password@myserver.com:5000");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseUri.resolve(uri);
+            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
+            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+            URI uri3 = new URI("/stores/locations?zip=12345");
+            URI resolvedUri1 = baseUri.resolve(uri1);
+            URI resolvedUri2 = baseUri.resolve(uri2);
+            URI resolvedUri3 = baseUri.resolve(uri3);
 
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
+            URL url1 = resolvedUri1.toURL();
+            System.out.println("URL 1 = " + url1);
+            URL url2 = resolvedUri2.toURL();
+            System.out.println("URL 2 = " + url2);
+            URL url3 = resolvedUri3.toURL();
+            System.out.println("URL 3 = " + url3);
 
 //            URI uri = new URI("hello");
-//            /*
+            /*
             System.out.println("Scheme = " + resolvedUri.getScheme());
             System.out.println("Scheme-specific part = " + resolvedUri.getSchemeSpecificPart());
             System.out.println("Authority = " + resolvedUri.getAuthority());
@@ -33,7 +41,7 @@ public class Main {
             System.out.println("Path = " + resolvedUri.getPath());
             System.out.println("Query = " + resolvedUri.getQuery());
             System.out.println("Fragment = " + resolvedUri.getFragment());
-//             */
+             */
 
         } catch (URISyntaxException ex) {
             System.out.println("URI Bad Syntax: " + ex.getMessage());
