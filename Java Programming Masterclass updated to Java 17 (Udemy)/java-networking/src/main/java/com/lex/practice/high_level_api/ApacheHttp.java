88ca4c0492fb2aade113b55750282c1a18211fed
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-03-27 23:16:31 +0800

    udemy java 17 ... finish networking part

--- Java Programming Masterclass updated to Java 17 (Udemy)/java-networking/src/main/java/com/lex/practice/high_level_api/ApacheHttp.java
+++ Java Programming Masterclass updated to Java 17 (Udemy)/java-networking/src/main/java/com/lex/practice/high_level_api/ApacheHttp.java
@@ -1,15 +1,13 @@
 
 package com.lex.practice.high_level_api;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author : LEX_YU
@@ -17,11 +15,24 @@ import java.util.Set;
  */
 public class ApacheHttp {
     public static void main(String[] args) {
-        try {
-
-
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet("https://example.org");
+            request.addHeader("User-Agent","Chrome");
+            try(CloseableHttpResponse response = httpClient.execute(request)) {
+                System.out.println("status code = " + response.getCode());
+                BufferedReader br =  new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                while (br.readLine() != null){
+                    String line = br.readLine();
+                    System.out.println(line);
+                }
+                br.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         } catch (Exception ex) {
-            ex.getMessage();
+            System.out.println(ex.getMessage());
         }
+
+
     }
 }
