dc4c5b92f047289069e3fd5277c408a12f9db3c2
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-11-06 18:22:30 +0800

    update reactive exercise 9

--- java-reactive-programming-demos/src/main/java/com/lex/exercises/Exercise9.java
+++ java-reactive-programming-demos/src/main/java/com/lex/exercises/Exercise9.java
@@ -1,25 +1,46 @@
 package com.lex.exercises;
 
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise9 {
 
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumbersFlux()
+		// Use ReactiveSources.intNumbersFlux()
 
-        // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+		// Print size of intNumbersFlux after the last item returns
+		// TODO: Write code here
+//		ReactiveSources.intNumbersFlux()
+//				.count()
+//				.subscribe(System.out::println);
 
-        // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+		// Collect all items of intNumbersFlux into a single list and print it
+		// TODO: Write code here
+//		ReactiveSources.intNumbersFlux()
+//				.collectList()
+//				.subscribe(System.out::println);
 
-        // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+//		List<Integer> list = ReactiveSources.intNumbersFlux()
+//				.collectList()
+//				.block();
+//		System.out.println(list);
 
-        System.out.println("Press a key to end");
-        System.in.read();
-    }
+		// Transform to a sequence of sums of adjacent two numbers
+		// TODO: Write code here
+//		Flux<List<Integer>> buffer = ReactiveSources.intNumbersFlux().buffer(3);
+//		buffer.collectList().subscribe(System.out::println);
+
+		ReactiveSources.intNumbersFlux()
+				.buffer(2)
+				.map(list -> list.get(0) + list.get(1))
+				.subscribe(System.out::println);
+
+		System.out.println("Press a key to end");
+		System.in.read();
+	}
 
 }
