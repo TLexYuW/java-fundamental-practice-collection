8d5de0f01655cb6c7652f232326a0f7affab9236
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-11-13 21:15:52 +0800

    update reactor context

--- java-reactive-programming-demos/src/main/java/com/lex/flux_mono/ReactorDemo1.java
+++ java-reactive-programming-demos/src/main/java/com/lex/flux_mono/ReactorDemo1.java
@@ -10,28 +10,37 @@ import java.util.stream.Stream;
  */
 public class ReactorDemo1 {
     public static void main(String[] args) {
-        ReactorSource.integerFlux().subscribe(System.out::println);
+
+//        ReactorSource.integerFlux().subscribe(System.out::println);
+
 
         System.out.println("==============================================");
 
         ReactorSource.stringFlux().subscribe(System.out::append);
 
         System.out.println("\n==============================================");
+        /*
         System.out.println("Array");
 
         Integer[] integers = ReactorSource.integers();
         Flux.fromArray(integers).subscribe(System.out::println);
+         */
 
         System.out.println("==============================================");
+        /*
         System.out.println("List");
 
         List<Integer> integerList = ReactorSource.integerList();
         Flux.fromIterable(integerList).subscribe(System.out::println);
+         */
 
         System.out.println("==============================================");
+        /*
         System.out.println("Stream");
 
         Stream<Integer> integerStream = ReactorSource.integerStream();
         Flux.fromStream(integerStream).subscribe(System.out::println);
+
+         */
     }
 }
