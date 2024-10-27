abc58296d638c5ab6fc3bfa92cde990308e1f2d7
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-10-27 13:48:34 +0800

    Update Block.java

--- java-sorted-collection/src/main/java/com/lex/practice/pdf/itext_demos/block/Block.java
+++ java-sorted-collection/src/main/java/com/lex/practice/pdf/itext_demos/block/Block.java
@@ -1,6 +1,7 @@
 package com.lex.practice.pdf.itext_demos.block;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
@@ -10,7 +11,7 @@ import com.itextpdf.layout.element.Paragraph;
 
 public class Block {
     public static void main(String[] args) throws FileNotFoundException {
-        PdfWriter writer = new PdfWriter("test");
+        PdfWriter writer = new PdfWriter("java-sorted-collection/src/main/resources/test.pdf");
         PdfDocument pdf = new PdfDocument(writer);
         try (Document document = new Document(pdf)) {
             document.add(new Paragraph("Hello World"));
